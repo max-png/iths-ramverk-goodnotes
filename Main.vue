289cb8b26740677ef7ae98cb96f87e34c6aeef77
Max Rune <max.rune@gmail.com>
2020-05-06 12:30:40 +0200

    Add main section

---
+++ Main.vue
@@ -0,0 +1,24 @@
+<template>
+<section>
+    <h2>
+        Main här
+    </h2>
+</section>
+</template>
+<script>
+export default {
+    data: () => ({
+
+    }),
+    
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+    section{
+        background-color: red;
+    }
+</style>
---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <Main/>
+    <h5>Text</h5>
+  </section>
+</template>
+
+<script>
+
+import Main from './Main'
+
+export default {
+  components: {
+    Main: Main
+  }
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+    background-color: lightgray;
+  }
+</style>
